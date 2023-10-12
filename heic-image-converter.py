658d02e703ec636f89a7cada4f7597fd57f05d06
nbenjwal@gmail.com <nbenjwal@gmail.com>
2023-10-12 22:55:49 +0530

    HEIC to JPEG and PNG converter

--- heic-image-converter.py
+++ heic-image-converter.py
@@ -2,14 +2,13 @@ from PIL import Image
 import pillow_heif
 import os
 
+
 ##################################################
 ## Author: nbenjwal
 ##################################################
-def convert_heic(heic_path, png_path, image_format):
-
-
-    if not os.path.exists(png_path):
-        os.makedirs(png_path)
+def convert_heic(heic_path, converted_images_directory, image_format):
+    if not os.path.exists(converted_images_directory):
+        os.makedirs(converted_images_directory)
 
     pillow_heif.register_heif_opener()  # You only need to register the opener once
 
@@ -18,13 +17,13 @@ def convert_heic(heic_path, png_path, image_format):
             if file.endswith(".HEIC"):
                 file_name = os.path.join(subdir, file)
                 img = Image.open(file_name)
-                output_file_name = os.path.join(png_path, f"{os.path.splitext(file)[0]}."+image_format.lower())
+                output_file_name = os.path.join(converted_images_directory, f"{os.path.splitext(file)[0]}." + image_format.lower())
                 img.save(output_file_name, format=image_format)
                 print(f"Converted: {output_file_name}")
 
 
 if __name__ == "__main__":
-    heic_images_path = 'F://DCIM'  # Path for HEIC format images
-    output_path = 'F://converted//'  # Path for converted images
-    required_image_format = 'JPEG'  # Supported: PNG, JPEG
-    convert_heic(heic_images_path, output_path, required_image_format)
+    heic_images_directory = 'F://DCIM'              # Directory holding the HEIC images
+    converted_images_directory = 'F://converted//'  # Destination directory for the converted images
+    target_format = 'PNG'                           # Desired output format (options: PNG, JPEG)
+    convert_heic(heic_images_directory, converted_images_directory, target_format)
